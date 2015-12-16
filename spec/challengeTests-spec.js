b9143e4821ec5c238423d5292ff0f47d5cb262b7
Andreas Nilsson <andreas.nilsson@bydeluxe.com>
2015-12-15 18:27:05 -0800

    Finished exercise 5 repeating xor

--- spec/challengeTests-spec.js
+++ spec/challengeTests-spec.js
@@ -1,6 +1,6 @@
-var HexToBaseSixtyFour = require("../app/hexToBaseSixtyFour.js");
 var xorIt = require("../app/XOR.js");
-//var fs = require('fs');
+//var dupes = require("../app/duplicateLines.js");
+
 
 describe("Challenge Tests", function () {
     /*
@@ -44,11 +44,67 @@ describe("Challenge Tests", function () {
     });
 */
 
+/*
     it('should decrypt message encrypted with single character XOR', function(){
         xorIt.decryptHidden(this,'/Users/nilssona/WebstormProjects/crypto/resources/4.txt',4.6).then(function(output){
             console.log(output);
         })
     });
+*/
+
+    /**
+     *
+     * Implement repeating-key XOR
+     * Here is the opening stanza of an important work of the English language:
+     * Burning 'em, if you ain't quick and nimble
+     * I go crazy when I hear a cymbal
+     * Encrypt it, under the key "ICE", using repeating-key XOR.
+     * In repeating-key XOR, you'll sequentially apply each byte of the key; the first byte of plaintext will be XOR'd against I, the next C, the next E, then I again for the 4th byte, and so on.
+     * It should come out to:
+     * 0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f
+     * Encrypt a bunch of stuff using your repeating-key XOR function. Encrypt your mail. Encrypt your password file. Your .sig file. Get a feel for it. I promise, we aren't wasting your time with this.
+     */
+    it('should encrypt a message using repeating-key XOR',function(){
+        runs(function(){
+            flag=false;
+            output = "";
+            xorIt.encryptRepeating(this,'/Users/nilssona/WebstormProjects/crypto/resources/5.txt','ICE').then(function(ret){
+                flag=true;
+                output = ret;
+            });
+        });
+
+        waitsFor(function(){
+            return flag;
+        },"waiting for file read to complete",500);
+
+        runs(function(){
+            console.log(output);
+            expect(output).toEqual("0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f");
+        });
+
+    });
+
+
+
+
+    //it('should encrypt a message using repeating-key XOR',function(){
+    //        xorIt.encryptRepeating2(this,'/Users/nilssona/WebstormProjects/crypto/resources/5.txt','ICE').then(function(output){
+    //        console.log("done "+ output);
+    //    });
+    //});
+
+
+    //it('should decrypt message encrypted with single character XOR', function(){
+    //    xorIt.justXor(this,'ICE').then(function(res){
+    //        console.log(res);
+    //        expect(res).toEqual("chuckle");
+    //    })
+    //});
+
+    //it('should test invalid hex character', function(){
+    //    xorIt.testLineFeed();
+    //});
 
 
 })
